090736047f23213679177cda972f43a3c895a621
tuyanga <168737904+tuyanga@users.noreply.github.com>
2025-02-16 21:26:48 +0800

    memory m+ m- mc ms mr method nemlee

--- Memoryfolder/Memory.cs
+++ Memoryfolder/Memory.cs
@@ -11,21 +11,41 @@ namespace CalculatorApp.Memoryfolder
         private List<MemoryItem> memoryList = new List<MemoryItem>();
 
         //ms
-        public void Save(MemoryItem item) 
+        public void MSave(double value) 
         {  
-            memoryList.Add(item);
+            memoryList.Add(new MemoryItem(value));
         }
         //mc
-        public void ClearMemory()
+        public void MClear()
         {
             memoryList.Clear();
         }
+        //madd
+        public void MAdd(double value)
+        {
+            if (memoryList.Count > 0)
+                memoryList[^1].MAdd(value);
+            else
+                MSave(value);
+        }
+
+        public void MSub(double value)
+        {
+            if (memoryList.Count > 0)
+                memoryList[^1].MSub(value);
+            else
+                MSave(-value);
+        }
+        public double MRecall()
+        {
+            return memoryList.Count > 0 ? memoryList[^1].Value : 0;
+        }
         public void Display()
         {
-            Console.WriteLine("Memory:");
+            Console.WriteLine("Memory list below");
             foreach(var item in memoryList)
             {
-                Console.WriteLine(item.Value);
+                Console.WriteLine($"Memory Item: {item.Value}");
             }
         }
         
