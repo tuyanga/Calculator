f95c7e9dd03d01a35fe54e31493fb517a7193f31
tuyanga <168737904+tuyanga@users.noreply.github.com>
2025-02-16 21:30:49 +0800

    program shalgah heseg

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace CalculatorApp
+﻿using CalculatorApp.Calculatorfoler;
+using CalculatorApp.Memoryfolder;
+namespace CalculatorApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calculator calc = new Calculator();
+            calc.Add(10);
+            Console.WriteLine("Result: "+ calc.Result);
+            calc.Sub(5);
+            Console.WriteLine("Result: " + calc.Result);
+            calc.MSave();
+            calc.Display();
+            calc.MAdd(2);
+            Console.WriteLine("Memory Recall: " + calc.MRecall());
+
+            calc.MSub(3);
+            Console.WriteLine("Memory Recall after MSub: " + calc.MRecall());
+
+            calc.MClear();
+            Console.WriteLine("Memory Cleared, Recall: " + calc.MRecall());
         }
     }
 }
