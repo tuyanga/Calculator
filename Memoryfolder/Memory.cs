685e2cea11d83dd9eddd845cc27107ee1ee2ad36
tuyanga <168737904+tuyanga@users.noreply.github.com>
2025-02-16 19:57:01 +0800

    memory class save clear display method added

--- Memoryfolder/Memory.cs
+++ Memoryfolder/Memory.cs
@@ -8,5 +8,26 @@ namespace CalculatorApp.Memoryfolder
 {
     internal class Memory
     {
+        private List<MemoryItem> memoryList = new List<MemoryItem>();
+
+        //ms
+        public void Save(MemoryItem item) 
+        {  
+            memoryList.Add(item);
+        }
+        //mc
+        public void ClearMemory()
+        {
+            memoryList.Clear();
+        }
+        public void Display()
+        {
+            Console.WriteLine("Memory:");
+            foreach(var item in memoryList)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+        
     }
 }
