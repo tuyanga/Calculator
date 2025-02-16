b39573518a7e1d4b78a1f6729a3018bce123c12e
tuyanga <168737904+tuyanga@users.noreply.github.com>
2025-02-16 21:29:13 +0800

    calc class dotor add sub memory iin uildliig nemsen

--- Calculatorfolder/Calculator.cs
+++ Calculatorfolder/Calculator.cs
@@ -3,12 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CalculatorApp.Memoryfolder;
 
 namespace CalculatorApp.Calculatorfoler
 {
     internal class Calculator :CalculatorAbs, IManagable
     {
-        public void Add(int num) { Result += num; }
-        public void Sub(int num) { Result -= num; }
+        public void Add(double num) 
+        { 
+            Result += num;
+        } 
+        public void Sub(double num) 
+        { 
+            Result -= num;
+        }
+        Memory memory=new Memory();
+        public void MSave()
+        {
+            memory.MSave(Result);
+        }
+
+        public void MAdd(double number)
+        {
+            memory.MAdd(number);
+        }
+
+        public void MSub(double number)
+        {
+            memory.MSub(number);
+        }
+
+        public void MClear()
+        {
+            memory.MClear();
+        }
+
+        public double MRecall()
+        {
+            return memory.MRecall();
+        }
+        public void Display()
+        {
+            memory.Display();
+        }
     }
 }
