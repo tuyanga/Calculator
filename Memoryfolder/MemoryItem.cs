514fbb76069bd8d3bce4838a1555b489a1b30058
tuyanga <168737904+tuyanga@users.noreply.github.com>
2025-02-16 21:27:47 +0800

    memory item tusad ni m+ m- uildel nemlee

--- Memoryfolder/MemoryItem.cs
+++ Memoryfolder/MemoryItem.cs
@@ -8,11 +8,20 @@ namespace CalculatorApp.Memoryfolder
 {
     internal class MemoryItem
     {   
-        public double Value { get; }
+        public double Value { get; private set; }
 
         public MemoryItem(double value) 
         { 
             Value = value; 
+        }
+        public void MAdd(double number)
+        {
+            Value += number;
+        }
+
+        public void MSub(double number)
+        {
+            Value -= number;
         }
     }
 }
