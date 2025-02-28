71abcf4ccd1fd64fa48cacefa5213c30e21fecba
tuyanga <168737904+tuyanga@users.noreply.github.com>
2025-02-28 13:16:00 +0800

    memory test

--- TestCalculator/Test_1.cs
+++ TestCalculator/Test_1.cs
@@ -1,17 +1,51 @@
-﻿using CalculatorLibrary.Calculatorfoler;
+﻿using CalculatorLibrary.Memoryfolder;
+
+using CalculatorLibrary.Calculatorfoler;
 
 namespace TestCalculator
 {
     [TestClass]
-    public sealed class Test1
-    {
-        [TestMethod]
-        public void Test_Add_Valid()
+        public class MemoryTests
         {
-            Calculator calc = new();
-            calc.Add(10);
-            Assert.AreEqual(10, calc.Result);
-            Console.WriteLine(calc.Result);
+            private Memory? memory;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            memory = new Memory();
         }
-    }
+        [TestMethod]
+        public void MSave_Store_MValue()
+            {
+                memory.MSave(10);
+                Assert.AreEqual(10, memory.MRecall());
+            }
+        [TestMethod]
+        public void MClear_Clear_Memory()
+            {
+                memory.MSave(10);
+                memory.MClear();
+                Assert.AreEqual(0, memory.MRecall());
+            }
+        [TestMethod]
+        public void MAdd_Increase_MValue()
+            {
+                memory.MSave(10);
+                memory.MAdd(5);
+                Assert.AreEqual(15, memory.MRecall());
+            }
+        [TestMethod]
+        public void MSub_Decrease_MValue()
+            {
+                memory.MSave(10);
+                memory.MSub(3);
+                Assert.AreEqual(7, memory.MRecall());
+            }
+        [TestMethod]
+        public void MRecall_ReturnZero()
+            {
+                Assert.AreEqual(0, memory.MRecall());
+            }
+        }
+
 }
