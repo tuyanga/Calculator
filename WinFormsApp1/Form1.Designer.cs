648ba7856651a820484812b4ab040143a15471c4
tuyanga <uka1898@gmail.com>
2025-03-23 16:43:43 +0800

    add form folder

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            num1 = new Button();
+            num2 = new Button();
+            num3 = new Button();
+            num4 = new Button();
+            num5 = new Button();
+            num6 = new Button();
+            num7 = new Button();
+            num8 = new Button();
+            equalbutton = new Button();
+            addbutton = new Button();
+            subbutton = new Button();
+            clearbutton = new Button();
+            btnMS = new Button();
+            memoryItemBindingSource = new BindingSource(components);
+            num9 = new Button();
+            num0 = new Button();
+            label1 = new Label();
+            txtResult = new TextBox();
+            memoryPanel = new FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)memoryItemBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // num1
+            // 
+            num1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            num1.Location = new Point(22, 604);
+            num1.Margin = new Padding(6);
+            num1.Name = "num1";
+            num1.Size = new Size(139, 126);
+            num1.TabIndex = 1;
+            num1.Text = "1";
+            num1.UseVisualStyleBackColor = true;
+            num1.Click += NumButton_Click;
+            // 
+            // num2
+            // 
+            num2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            num2.Location = new Point(173, 604);
+            num2.Margin = new Padding(6);
+            num2.Name = "num2";
+            num2.Size = new Size(139, 126);
+            num2.TabIndex = 2;
+            num2.Text = "2";
+            num2.UseVisualStyleBackColor = true;
+            num2.Click += NumButton_Click;
+            // 
+            // num3
+            // 
+            num3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            num3.Location = new Point(323, 604);
+            num3.Margin = new Padding(6);
+            num3.Name = "num3";
+            num3.Size = new Size(139, 126);
+            num3.TabIndex = 3;
+            num3.Text = "3";
+            num3.UseVisualStyleBackColor = true;
+            num3.Click += NumButton_Click;
+            // 
+            // num4
+            // 
+            num4.Font = new Font("Segoe UI", 16F);
+            num4.Location = new Point(22, 465);
+            num4.Margin = new Padding(6);
+            num4.Name = "num4";
+            num4.Size = new Size(139, 126);
+            num4.TabIndex = 4;
+            num4.Text = "4";
+            num4.UseVisualStyleBackColor = true;
+            num4.Click += NumButton_Click;
+            // 
+            // num5
+            // 
+            num5.Font = new Font("Segoe UI", 16F);
+            num5.Location = new Point(173, 465);
+            num5.Margin = new Padding(6);
+            num5.Name = "num5";
+            num5.Size = new Size(139, 126);
+            num5.TabIndex = 6;
+            num5.Text = "5";
+            num5.UseVisualStyleBackColor = true;
+            num5.Click += NumButton_Click;
+            // 
+            // num6
+            // 
+            num6.Font = new Font("Segoe UI", 16F);
+            num6.Location = new Point(323, 465);
+            num6.Margin = new Padding(6);
+            num6.Name = "num6";
+            num6.Size = new Size(139, 126);
+            num6.TabIndex = 5;
+            num6.Text = "6";
+            num6.UseVisualStyleBackColor = true;
+            num6.Click += NumButton_Click;
+            // 
+            // num7
+            // 
+            num7.Font = new Font("Segoe UI", 16F);
+            num7.Location = new Point(22, 326);
+            num7.Margin = new Padding(6);
+            num7.Name = "num7";
+            num7.Size = new Size(139, 126);
+            num7.TabIndex = 9;
+            num7.Text = "7";
+            num7.UseVisualStyleBackColor = true;
+            num7.Click += NumButton_Click;
+            // 
+            // num8
+            // 
+            num8.Font = new Font("Segoe UI", 16F);
+            num8.Location = new Point(173, 326);
+            num8.Margin = new Padding(6);
+            num8.Name = "num8";
+            num8.Size = new Size(139, 126);
+            num8.TabIndex = 8;
+            num8.Text = "8";
+            num8.UseVisualStyleBackColor = true;
+            num8.Click += NumButton_Click;
+            // 
+            // equalbutton
+            // 
+            equalbutton.BackColor = Color.MediumSlateBlue;
+            equalbutton.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            equalbutton.Location = new Point(173, 742);
+            equalbutton.Margin = new Padding(6);
+            equalbutton.Name = "equalbutton";
+            equalbutton.Size = new Size(290, 126);
+            equalbutton.TabIndex = 10;
+            equalbutton.Text = "=";
+            equalbutton.UseVisualStyleBackColor = false;
+            equalbutton.Click += equalbutton_Click;
+            // 
+            // addbutton
+            // 
+            addbutton.BackColor = SystemColors.Control;
+            addbutton.Font = new Font("Segoe UI", 16F);
+            addbutton.Location = new Point(474, 742);
+            addbutton.Margin = new Padding(6);
+            addbutton.Name = "addbutton";
+            addbutton.Size = new Size(139, 126);
+            addbutton.TabIndex = 11;
+            addbutton.Text = "+";
+            addbutton.UseVisualStyleBackColor = false;
+            addbutton.Click += addbutton_Click;
+            // 
+            // subbutton
+            // 
+            subbutton.BackColor = SystemColors.Control;
+            subbutton.Font = new Font("Segoe UI", 16F);
+            subbutton.Location = new Point(474, 604);
+            subbutton.Margin = new Padding(6);
+            subbutton.Name = "subbutton";
+            subbutton.Size = new Size(139, 126);
+            subbutton.TabIndex = 12;
+            subbutton.Text = "-";
+            subbutton.UseVisualStyleBackColor = false;
+            subbutton.Click += subbutton_Click;
+            // 
+            // clearbutton
+            // 
+            clearbutton.BackColor = SystemColors.Control;
+            clearbutton.Font = new Font("Segoe UI", 12F);
+            clearbutton.Location = new Point(474, 465);
+            clearbutton.Margin = new Padding(6);
+            clearbutton.Name = "clearbutton";
+            clearbutton.Size = new Size(139, 126);
+            clearbutton.TabIndex = 13;
+            clearbutton.Text = "C";
+            clearbutton.UseVisualStyleBackColor = false;
+            clearbutton.Click += clearbutton_Click;
+            // 
+            // btnMS
+            // 
+            btnMS.BackColor = SystemColors.Control;
+            btnMS.Font = new Font("Segoe UI", 12F);
+            btnMS.Location = new Point(474, 326);
+            btnMS.Margin = new Padding(6);
+            btnMS.Name = "btnMS";
+            btnMS.Size = new Size(139, 126);
+            btnMS.TabIndex = 14;
+            btnMS.Text = "MS";
+            btnMS.UseVisualStyleBackColor = false;
+            btnMS.Click += btnMS_Click;
+            // 
+            // memoryItemBindingSource
+            // 
+            memoryItemBindingSource.DataSource = typeof(CalculatorLibrary.Memoryfolder.MemoryItem);
+            // 
+            // num9
+            // 
+            num9.Font = new Font("Segoe UI", 16F);
+            num9.Location = new Point(323, 326);
+            num9.Margin = new Padding(6);
+            num9.Name = "num9";
+            num9.Size = new Size(139, 126);
+            num9.TabIndex = 16;
+            num9.Text = "9";
+            num9.UseVisualStyleBackColor = true;
+            num9.Click += NumButton_Click;
+            // 
+            // num0
+            // 
+            num0.Font = new Font("Segoe UI", 16F);
+            num0.Location = new Point(22, 742);
+            num0.Margin = new Padding(6);
+            num0.Name = "num0";
+            num0.Size = new Size(139, 126);
+            num0.TabIndex = 17;
+            num0.Text = "0";
+            num0.UseVisualStyleBackColor = true;
+            num0.Click += NumButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(648, 19);
+            label1.Margin = new Padding(6, 0, 6, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 45);
+            label1.TabIndex = 18;
+            label1.Text = "Memory";
+            // 
+            // txtResult
+            // 
+            txtResult.AcceptsTab = true;
+            txtResult.Font = new Font("Segoe UI", 16F);
+            txtResult.Location = new Point(22, 85);
+            txtResult.Margin = new Padding(6);
+            txtResult.Multiline = true;
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(587, 209);
+            txtResult.TabIndex = 20;
+            txtResult.Text = "0";
+            txtResult.TextAlign = HorizontalAlignment.Right;
+            // 
+            // memoryPanel
+            // 
+            memoryPanel.AutoScroll = true;
+            memoryPanel.Location = new Point(670, 85);
+            memoryPanel.Margin = new Padding(6);
+            memoryPanel.Name = "memoryPanel";
+            memoryPanel.Size = new Size(555, 783);
+            memoryPanel.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(1265, 929);
+            Controls.Add(memoryPanel);
+            Controls.Add(txtResult);
+            Controls.Add(label1);
+            Controls.Add(num0);
+            Controls.Add(num9);
+            Controls.Add(btnMS);
+            Controls.Add(clearbutton);
+            Controls.Add(subbutton);
+            Controls.Add(addbutton);
+            Controls.Add(equalbutton);
+            Controls.Add(num7);
+            Controls.Add(num8);
+            Controls.Add(num5);
+            Controls.Add(num6);
+            Controls.Add(num4);
+            Controls.Add(num3);
+            Controls.Add(num2);
+            Controls.Add(num1);
+            Margin = new Padding(6);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)memoryItemBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button num1;
+        private Button num2;
+        private Button num3;
+        private Button num4;
+        private Button num5;
+        private Button num6;
+        private Button num7;
+        private Button num8;
+        private Button equalbutton;
+        private Button addbutton;
+        private Button subbutton;
+        private Button clearbutton;
+        private Button btnMS;
+        private BindingSource memoryItemBindingSource;
+        private Button num9;
+        private Button num0;
+        private Label label1;
+        private TextBox txtResult;
+        private FlowLayoutPanel memoryPanel;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using CalculatorLibrary.Calculatorfoler;
+using CalculatorLibrary.Memoryfolder;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Calculator calc = new Calculator();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private char operation = ' ';
+        private bool entervalue = false;
+
+        private void NumButton_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (txtResult.Text == "0" || entervalue)
+            {
+                txtResult.Text = btn.Text;
+                entervalue = false;
+            }
+            else
+            {
+                txtResult.Text += btn.Text;
+            }
+        }
+
+        private void addbutton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtResult.Text, out int num))
+            {
+                calc.Result = num;
+                operation = '+';
+                txtResult.Clear();
+                entervalue = true;
+            }
+        }
+
+        private void subbutton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtResult.Text, out int num))
+            {
+                calc.Result = num;
+                operation = '-';
+                txtResult.Clear();
+                entervalue = true;
+            }
+        }
+
+        private void clearbutton_Click(object sender, EventArgs e)
+        {
+            txtResult.Clear();
+            calc.Result = 0;
+            operation = ' ';
+        }
+        private void equalbutton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtResult.Text, out int num))
+            {
+                if (operation == '+')
+                    calc.Add(num);
+                else if (operation == '-')
+                    calc.Sub(num);
+
+                txtResult.Text = calc.Result.ToString();
+            }
+            entervalue = true;
+        }
+        private void btnMS_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtResult.Text, out int value))
+            {
+                calc.Result = value;
+                MemoryItem memItem = calc.MSave();
+                AddMemoryItem(memItem);
+            }
+            entervalue = true;
+        }
+
+
+        private void AddMemoryItem(MemoryItem memItem)
+        {
+            Panel memoryItemPanel = new Panel
+            {
+                Height = 50,
+                Width = memoryPanel.Width - 5,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            Label lblValue = new Label
+            {
+                Text = memItem.Value.ToString(),
+                AutoSize = true,
+                Location = new Point(5, 5)
+            };
+
+            Button btnMC = new Button
+            {
+                Text = "MC",
+                Width = 60,
+                Height = 45,
+                Location = new Point(85, 2)
+            };
+            btnMC.Click += (s, e) => { memoryPanel.Controls.Remove(memoryItemPanel); };
+
+            Button btnMPlus = new Button
+            {
+                Text = "M+",
+                Width = 60,
+                Height = 45,
+                Location = new Point(150, 2)
+            };
+            btnMPlus.Click += (s, e) =>
+            {
+                if (int.TryParse(txtResult.Text, out int inputValue))
+                {
+                    memItem.MAdd(inputValue);
+                    lblValue.Text = memItem.Value.ToString();
+                }
+                entervalue = true;
+            };
+
+            Button btnMMinus = new Button
+            {
+                Text = "M-",
+                Width = 60,
+                Height = 45,
+                Location = new Point(215, 2)
+            };
+            btnMMinus.Click += (s, e) =>
+            {
+                if (int.TryParse(txtResult.Text, out int inputValue))
+                {
+                    memItem.MSub(inputValue);
+                    lblValue.Text = memItem.Value.ToString();
+                }
+                entervalue = true;
+            };
+
+            memoryItemPanel.Controls.Add(lblValue);
+            memoryItemPanel.Controls.Add(btnMC);
+            memoryItemPanel.Controls.Add(btnMPlus);
+            memoryItemPanel.Controls.Add(btnMMinus);
+
+            memoryPanel.Controls.Add(memoryItemPanel);
+            memoryPanel.Height = 800;
+
+            if (memoryPanel.Controls.Count * 70 > 800)
+            {
+                memoryPanel.AutoScroll = true;
+            }
+            else
+            {
+                memoryPanel.AutoScroll = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            memoryPanel.AutoScroll = true;
+        }
+    }
+}
